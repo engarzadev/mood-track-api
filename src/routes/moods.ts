8eb5b0dbd91673344ccb66ee865818000e1cef08
elisabeth <engarzadev@gmail.com>
2025-05-20 15:43:10 -0400

    route warnings

--- src/routes/moods.ts
+++ src/routes/moods.ts
@@ -3,12 +3,12 @@ import Mood from "../models/Mood";
 
 const router = express.Router();
 
-router.get("/", async (_, res) => {
+router.get("/", async (_: express.Request, res: express.Response) => {
   const moods = await Mood.find().sort({ date: -1 });
   res.json(moods);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", async (req: express.Request, res: express.Response) => {
   const mood = await Mood.create(req.body);
   res.json(mood);
 });
