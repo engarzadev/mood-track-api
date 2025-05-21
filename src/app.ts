756a737a010a49782b8ce6413dae58c9faf4fbbf
elisabeth <engarzadev@gmail.com>
2025-05-20 21:01:35 -0400

    Fix: CORS config for Vercel frontend

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,18 @@ import cors from "cors";
 import dotenv from "dotenv";
 import moodRoutes from "./routes/moods";
 
+const allowedOrigins = [
+  "http://localhost:5173", // for local dev
+  "https://mood-track-ebd7m46bi-engarzadevs-projects.vercel.app/" // update this
+];
+
 dotenv.config();
 const app = express();
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "OPTIONS"],
+  credentials: true
+}));app.use(express.json());
 app.use("/moods", moodRoutes);
 
 mongoose.connect(process.env.MONGO_URI!).then(() => console.log("MongoDB connected"));
