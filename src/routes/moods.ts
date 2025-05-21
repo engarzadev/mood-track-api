1d58d2b46cd60e1a3d48070f5c68700903acf25c
elisabeth <engarzadev@gmail.com>
2025-05-20 21:57:10 -0400

    Fix: invalid route path crashing server

--- src/routes/moods.ts
+++ src/routes/moods.ts
@@ -3,12 +3,12 @@ import Mood from "../models/Mood";
 
 const router = express.Router();
 
-router.get("/", async (_: express.Request, res: express.Response) => {
+router.get("/", async (_, res) => {
   const moods = await Mood.find().sort({ date: -1 });
   res.json(moods);
 });
 
-router.post("/", async (req: express.Request, res: express.Response) => {
+router.post("/", async (req, res) => {
   const mood = await Mood.create(req.body);
   res.json(mood);
 });
